aca5a451118a162675c5a52eebd82ae6a55801c2
DevTapGym <2001224389@huit.edu.vn>
2025-11-19 21:43:08 +0700

    add created_by for event

--- app/GraphQL/Queries/EventResolver.php
+++ app/GraphQL/Queries/EventResolver.php
@@ -12,7 +12,7 @@ class EventResolver
         try {
             return Event::all();
         } catch (Exception $e) {
-            throw new Exception('Internal server error: ' . $e->getMessage());
+            throw new Exception('Internal server error: '.$e->getMessage());
         }
     }
 
@@ -20,12 +20,31 @@ class EventResolver
     {
         try {
             $event = Event::find($args['id']);
-            if (!$event) {
+            if (! $event) {
                 throw new Exception('Event not found');
             }
+
             return $event;
         } catch (Exception $e) {
-            throw new Exception('Internal server error: ' . $e->getMessage());
+            throw new Exception('Internal server error: '.$e->getMessage());
+        }
+    }
+
+    public function byCreator($_, array $args)
+    {
+        try {
+            $email = $args['email'];
+
+            // Kiểm tra email có tồn tại không
+            $events = Event::where('created_by', $email)->get();
+
+            if ($events->isEmpty()) {
+                throw new Exception('Không tìm thấy sự kiện nào được tạo bởi email: '.$email);
+            }
+
+            return $events;
+        } catch (Exception $e) {
+            throw new Exception($e->getMessage());
         }
     }
 }
